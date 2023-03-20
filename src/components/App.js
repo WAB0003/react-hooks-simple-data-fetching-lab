882a417bdaadb7d8382176b996936ca4bdb47789
Bill Brown <bill.a.brown90@gmail.com>
2023-03-20 12:24:19 -0700

    simple fetch in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App () {
+    const dogURL = "https://dog.ceo/api/breeds/image/random"
+    const[currentImg,setCurrentImg]=useState("")
+    const[isLoaded, setIsLoaded] = useState(false)
+    
+    useEffect(()=> {
+        fetch(dogURL)
+        .then((resp)=>resp.json())
+        .then((data)=>{
+            setCurrentImg(data.message)
+            setIsLoaded(true)
+        })
+    },[])
+    function displayDogs(){
+        if(!isLoaded) return <p>Loading...</p>;
+
+        return <img src={currentImg} alt="A Random Dog"/>
+    }
+
+    return(
+        <div>
+            {displayDogs()}
+       </div>
+    )
+}
+
+export default App;
